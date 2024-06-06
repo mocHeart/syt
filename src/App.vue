87e5930b201d5f2674052106a8fd3520cb5e44a1
mocHeart <nuc2015@163.com>
2024-06-06 20:20:59 +0800

    first commit.

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>APP</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+// 引入node提供内置模块path：可以获取绝对路径
+import path from 'path';
+
+export default defineConfig({
+  plugins: [vue()],
+  // src文件夹别名配置
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
+})
