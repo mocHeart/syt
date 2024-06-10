f6c57bb393baec646f34a3d0e181ee132c8a66c6
jianyaoli <nuc2015@163.com>
2024-06-10 10:43:29 +0800

    首页基本框架搭建

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 ### 说明
 此仓库是学校尚硅谷Vue3实战项目商医通的学习代码，仅供参考。
+
+### 接口
+服务器地址:http://syt.atguigu.cn
+医院接口：http://139.198.34.216:8201/swagger-ui.html
+公共数据接口：http://139.198.34.216:8202/swagger-ui.html
+会员接口：http://139.198.34.216:8203/swagger-ui.html
+短信验证码接口：http://139.198.34.216:8204/swagger-ui.html
+订单接口：http://139.198.34.216:8206/swagger-ui.html
+文件上传接口：http://139.198.34.216:8205/swagger-ui.html
+后台用户接口：http://139.198.34.216:8212/swagger-ui.html
--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
 <template>
-  <div>
-    <h1>APP</h1>
+  <div class="container">
+    <!-- 顶部全局组件 -->
+    <HospitalTop />
+    <!-- 展示路由组件的区域 -->
+     <div class="content">
+        hhhhhaaahhhaaa
+     </div>
+     <HospitalBottom />
   </div>
 </template>
 
 <script setup lang="ts">
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.container {
+  display: flex; 
+  flex-direction: column;
+  align-items: center;
+  .content {
+    margin-top: 70px;
+    width: 1200px;
+    min-height: 700px;
+    background-color: #bcf;
+  }
+}
 </style>
