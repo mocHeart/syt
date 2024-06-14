3a014ddd3a1cf7bedbb48e3c7e729ca6d25169d0
mocHeart <nuc2015@163.com>
2024-06-14 17:05:06 +0800

    首页搜索框搜索医院及跳转

--- src/components/hospital_top/index.vue
+++ src/components/hospital_top/index.vue
@@ -2,20 +2,28 @@
   <div class="top">
     <div class="content">
       <!-- 左侧 -->
-       <div class="left">
-          <img src="../../assets/images/logo.png" alt="" srcset="">
-          <p>STY 预约挂号统一平台</p>
-       </div>
-       <!-- 右侧 -->
-        <div class="right">
-          <p class="help">帮助中心</p>
-          <p class="login">登录/注册</p>
-        </div>
+      <div class="left" @click="goHome">
+        <img src="../../assets/images/logo.png" alt="" srcset="" />
+        <p>STY 预约挂号统一平台</p>
+      </div>
+      <!-- 右侧 -->
+      <div class="right">
+        <p class="help">帮助中心</p>
+        <p class="login">登录/注册</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { useRouter } from "vue-router";
+
+let $router = useRouter();
+
+const goHome = () => {
+  // 编程式导航跳转到首页
+  $router.push({ path: "/home" });
+};
 </script>
 
 <style scoped lang="scss">
