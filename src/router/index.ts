bb8e4ddcaa3bd94af16b71a92bd405a2b5f4e111
mocHeart <nuc2015@163.com>
2024-06-14 17:31:24 +0800

    医院详情路由（未完成）

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,23 +7,76 @@ export default createRouter({
   // 管理路由
   routes: [
     {
-      path: '/home',
-      component: () => import('@/pages/home/index.vue')
+      path: "/home",
+      component: () => import("@/pages/home/index.vue"),
     },
     {
-      path: '/hospital',
-      component: () => import('@/pages/hospital/index.vue')
+      path: "/hospital",
+      component: () => import("@/pages/hospital/index.vue"),
+      children: [
+        {
+          path: "register",
+          component: () => import("@/pages/hospital/register/index.vue"),
+          meta: {
+            title: "预约挂号",
+          },
+        },
+        {
+          path: "detail",
+          component: () => import("@/pages/hospital/detail/index.vue"),
+          meta: {
+            title: "医院详情",
+          },
+        },
+        {
+          path: "notice",
+          component: () => import("@/pages/hospital/notice/index.vue"),
+          meta: {
+            title: "预约通知",
+          },
+        },
+        {
+          path: "close",
+          component: () => import("@/pages/hospital/close/index.vue"),
+          meta: {
+            title: "停诊信息",
+          },
+        },
+        {
+          path: "search",
+          component: () => import("@/pages/hospital/search/index.vue"),
+          meta: {
+            title: "查询",
+          },
+        },
+        {
+          path: "register_step1",
+          component: () =>
+            import("@/pages/hospital/register/register_step1.vue"),
+          meta: {
+            title: "预约第一步",
+          },
+        },
+        {
+          path: "register_step2",
+          component: () =>
+            import("@/pages/hospital/register/register_step2.vue"),
+          meta: {
+            title: "预约第二步",
+          },
+        },
+      ],
     },
     {
-      path: '/',
-      redirect: '/home'
-    }
+      path: "/",
+      redirect: "/home",
+    },
   ],
   // 滚动行为
   scrollBehavior() {
     return {
       left: 0,
-      top: 0
-    }
-  }
-})
+      top: 0,
+    };
+  },
+});
