2ec38539015b4015380fe2f87db65dbc52ec34db
jianyaoli <nuc2015@163.com>
2024-06-15 10:09:30 +0800

    医院详情模块-子模块详情/通知等

--- src/pages/hospital/search/index.vue
+++ src/pages/hospital/search/index.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>111</div>
+  <div>
+    <div class="search">
+      <h1>{{ hospitalStore.hospitalInfo.hospital?.hosname }}-查询/取消</h1>
+      <el-empty description="暂无信息" />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+//引入医院详情仓库的数据
+import useDetailStore from "@/store/modules/hospitalDetail";
+
+let hospitalStore = useDetailStore();
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.search {
+  h1 {
+    text-align: center;
+    font-size: 30px;
+  }
+}
 </style>
